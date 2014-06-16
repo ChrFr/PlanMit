56499eb5032f41472a7aad1ff7628554cde7f286
Christoph Franke <christoph.franke@web.de>
2014-06-16 09:40:13 +0200

    Menu

--- public/js/app/init/DesktopInit.js
+++ public/js/app/init/DesktopInit.js
@@ -6,6 +6,11 @@ require(["jquery", "backbone", "routers/DesktopRouter",
     "jqueryui", "bootstrap", "backbone.validateAll"],
 
   function($, Backbone, DesktopRouter) {
+    //change active item if navbar item is clicked
+    $('ul.nav > li').click(function (e) {
+            $('ul.nav > li').removeClass('active');
+            $(this).addClass('active');                
+    });     
     // Instantiates a new Desktop Router instance
     new DesktopRouter();
 
